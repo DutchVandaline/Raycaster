3c708839cc9cfb2498173629c8ebd8d586a17de9
Jason Park <142364450+DutchVandaline@users.noreply.github.com>
2024-04-29 22:58:07 +0900

    Create sprites.h

---
+++ sprites.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef SPRITES_H
+#define SPRITES_H
+
+extern int playerSprite1[32][32];
+extern int playerSprite2[32][32];
+extern int zoomSprite1[32][32];
+extern int zoomSprite2[32][32];
+extern int fireSprite[32][32];
+extern int All_Textures[];
+
+
+#endif
