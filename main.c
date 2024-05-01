b422b92c7d7d71e98c3b73660cc34cc6fbe618ca
Jason Park <142364450+DutchVandaline@users.noreply.github.com>
2024-05-01 20:47:30 +0900

    added monster sprite

--- main.c
+++ main.c
@@ -136,21 +136,11 @@ void drawFireSprite() {
 
     for (int y = 0; y < spriteSize; y++) {
         for (int x = 0; x < spriteSize; x++) {
-            if ((fireSprite)[y][x] == 1) { // If the value is 1, color white
-                glColor3f(1.0, 1.0, 1.0);
-            }
-            else if ((fireSprite)[y][x] == 2) { // If the value is 2, color black
-                glColor3f(0.0, 0.0, 0.0);
-            }
-            else if ((fireSprite)[y][x] == 3) { // If the value is 2, color black
-                glColor3f(1.0, 0.0, 0.0);
-            }
-            else if ((fireSprite)[y][x] == 4) { // If the value is 2, color black
-                glColor3f(1.0, 1.0, 0.0);
-            }
-            else {
-                continue;
-            }
+            if ((fireSprite)[y][x] == 1) {glColor3f(1.0, 1.0, 1.0);} // If the value is 1, color white
+            else if ((fireSprite)[y][x] == 2) { glColor3f(0.0, 0.0, 0.0);}// If the value is 2, color black
+            else if ((fireSprite)[y][x] == 3) {glColor3f(1.0, 0.0, 0.0);} // If the value is 2, color black
+            else if ((fireSprite)[y][x] == 4) {glColor3f(1.0, 1.0, 0.0);} // If the value is 2, color black
+            else {continue;}
             glBegin(GL_QUADS);
             glVertex2i(screenPosX + x * pixelSize, screenPosY + y * pixelSize);
             glVertex2i(screenPosX + x * pixelSize + pixelSize, screenPosY + y * pixelSize);
@@ -163,10 +153,87 @@ void drawFireSprite() {
 }
 
 
+typedef struct
+{
+    int type;       //static, key, enemy
+    int state;      //on, off
+    int map;        //texture to show
+    double x, y, z;    //position
+}sprite; sprite sp[4];
+int depth[120];
+
+
+void drawMonsterSprite()
+{
+    int x, y, s;
+    //if (px < sp[0].x + 10 && px > sp[0].x - 10 && py < sp[0].y + 10 && py > sp[0].y - 10) {sp[0].state = 0;} //pick up key
+
+    int spx = (int)sp[0].x >> 6, spy = (int)sp[0].y >> 6;
+    int spx_add = ((int)sp[0].x + 15) >> 6, spy_add = ((int)sp[0].y + 15) >> 6;
+    int spx_sub = ((int)sp[0].x - 15) >> 6, spy_sub = ((int)sp[0].y - 15) >> 6;
+
+    if (sp[0].x > px && mapW[spy * 8 + spx_sub] == 0) { sp[0].x -= 0.03 * fps; }
+    if (sp[0].x < px && mapW[spy * 8 + spx_add] == 0) { sp[0].x += 0.03 * fps; }
+    if (sp[0].y > py && mapW[spy_sub * 8 + spx] == 0) { sp[0].y -= 0.03 * fps; }
+    if (sp[0].y < py && mapW[spy_add * 8 + spx] == 0) { sp[0].y += 0.03 * fps; }
+
+
+    for (s = 0; s < 1; s++) {
+
+        float sx = sp[s].x - px;
+        float sy = sp[s].y - py;
+        float sz = sp[s].z;
+
+        float CS = cos(degToRad(pa)), SN = sin(degToRad(pa)); // rotate based on player side
+        float a = sy * CS + sx * SN;
+        float b = sx * CS - sy * SN;
+        sx = a; sy = b;
+
+        sx = (sx * 108.0 / sy) + (120 / 2); //convert to screen 
+        sy = (sz * 108.0 / sy) + (80 / 2);
+
+        int scale = 45 * 80 / b;
+        if (scale < 0) { scale = 0; }
+        if (scale > 120) { scale = 120; }
+
+        //texture
+        float t_x = 0, t_y = 31, t_x_step = 32.0 / (float) scale, t_y_step = 32.0/(float) scale;
+
+        for (x = sx - scale / 2; x < sx + scale / 2; x++)
+        {
+            t_y = 31;
+            for (y = 0; y < scale; y++) 
+            {
+                if (x > 0 && x < 120 && b < depth[x]) //check the depth and see draw if it's on screen.
+                {
+                    
+                    int pixel = ((int)t_y * 32 + (int)t_x) * 3;
+                    int red = monster_walkSprite1[pixel + 0];
+                    int green = monster_walkSprite1[pixel + 1];
+                    int blue = monster_walkSprite1[pixel + 2];
+                    if (red != 255, green != 255, blue != 255) {
+                        glPointSize(8);
+                        glColor3ub(red, green, blue);
+                        glBegin(GL_POINTS);
+                        glVertex2i(x * 8, sy * 8 - y * 8);
+                        glEnd();
+                    }
+                    
+                    t_y -= t_y_step; if (t_y < 0) { t_y = 0; }
+                }
+            }
+            t_x += t_x_step;
+        }
+    }
+}
+
+
+
 //---------------------------Draw Rays and Walls--------------------------------
 void drawRays2D()
 {
-    int r, mx, my, mp, dof, side; float vx, vy, rx, ry, ra, xo, yo, disV, disH;
+    int r, mx, my, mp, dof, side;
+    float vx, vy, rx, ry, ra, xo, yo, disV, disH;
 
     ra = FixAng(pa + 30);                                                              //ray set back 30 degrees
 
@@ -176,8 +243,16 @@ void drawRays2D()
         //---Vertical--- 
         dof = 0; side = 0; disV = 100000;
         float Tan = tan(degToRad(ra));
-        if (cos(degToRad(ra)) > 0.001) { rx = (((int)px >> 6) << 6) + 64;      ry = (px - rx) * Tan + py; xo = 64; yo = -xo * Tan; }//looking left
-        else if (cos(degToRad(ra)) < -0.001) { rx = (((int)px >> 6) << 6) - 0.0001; ry = (px - rx) * Tan + py; xo = -64; yo = -xo * Tan; }//looking right
+        if (cos(degToRad(ra)) > 0.001) { rx = (((int)px >> 6) << 6) + 64;
+        ry = (px - rx) * Tan + py; 
+        xo = 64; yo = -xo * Tan; }//looking left
+
+        else if (cos(degToRad(ra)) < -0.001) { 
+            rx = (((int)px >> 6) << 6) - 0.0001; 
+            ry = (px - rx) * Tan + py; 
+            xo = -64; 
+            yo = -xo * Tan; }//looking right
+
         else { rx = px; ry = py; dof = 8; }                                                  //looking up or down. no hit  
 
         while (dof < 8)
@@ -212,6 +287,8 @@ void drawRays2D()
         float ty_off = 0;
         if (lineH > 640) { ty_off = (lineH - 640) / 2.0; lineH = 640; }                            //line height and limit
         int lineOff = 320 - (lineH >> 1);                                               //line offset
+
+        depth[r] = disH;
 
         //---draw walls---
         int y;
@@ -257,12 +334,15 @@ void init()
     glClearColor(0.3, 0.3, 0.3, 0);
     gluOrtho2D(0, 960, 640, 0);
     px = 150; py = 400; pa = 90;
-    pdx = cos(degToRad(pa)); pdy = -sin(degToRad(pa));                                 //init player
+    pdx = cos(degToRad(pa)); pdy = -sin(degToRad(pa));  //init player
+
+    sp[0].type = 1; sp[0].state = 1;sp[0].map= 0;sp[0].x = 1.5 * 64;sp[0].y = 5 * 64;sp[0].z = 20;
 }
 
 
 void display()
 {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     //frames per second
     frame2 = glutGet(GLUT_ELAPSED_TIME);
     fps = (frame2 - frame1);
@@ -281,29 +361,31 @@ void display()
     }
 
     //buttons
-    if (Keys.a == 1) { pa += 0.2 * fps; pa = FixAng(pa); pdx = cos(degToRad(pa)); pdy = -sin(degToRad(pa)); }
-    if (Keys.d == 1) { pa -= 0.2 * fps; pa = FixAng(pa); pdx = cos(degToRad(pa)); pdy = -sin(degToRad(pa)); }
+    double moveRate = 0.0;
+    if (showZoomSprite == 1) moveRate = 0.1; else moveRate = 0.2;                 //slow down when zoom
+    if (Keys.a == 1) { pa += moveRate * fps; pa = FixAng(pa); pdx = cos(degToRad(pa)); pdy = -sin(degToRad(pa));}
+    if (Keys.d == 1) { pa -= moveRate * fps; pa = FixAng(pa); pdx = cos(degToRad(pa)); pdy = -sin(degToRad(pa)); }
 
     int xo = 0; if (pdx < 0) { xo = -20; }else { xo = 20; }                                    //x offset to check map
     int yo = 0; if (pdy < 0) { yo = -20; }else { yo = 20; }                                    //y offset to check map
-    int ipx = px / 64.0, ipx_add_xo = (px + xo) / 64.0, ipx_sub_xo = (px - xo) / 64.0;             //x position and offset
-    int ipy = py / 64.0, ipy_add_yo = (py + yo) / 64.0, ipy_sub_yo = (py - yo) / 64.0;             //y position and offset
+    int ipx = px / 64.0, ipx_add_xo = (px + xo) / 64.0, ipx_sub_xo = (px - xo) / 64.0;         //x position and offset
+    int ipy = py / 64.0, ipy_add_yo = (py + yo) / 64.0, ipy_sub_yo = (py - yo) / 64.0;         //y position and offset
+    
     if (Keys.w == 1)                                                                  //move forward
-    {
-        if (mapW[ipy * mapX + ipx_add_xo] == 0) { px += pdx * 0.2 * fps; }
-        if (mapW[ipy_add_yo * mapX + ipx] == 0) { py += pdy * 0.2 * fps; }
+    {   
+        if (mapW[ipy * mapX + ipx_add_xo] == 0) { px += pdx * moveRate * fps; }
+        if (mapW[ipy_add_yo * mapX + ipx] == 0) { py += pdy * moveRate * fps; }
     }
     if (Keys.s == 1)                                                                  //move backward
     {
-        if (mapW[ipy * mapX + ipx_sub_xo] == 0) { px -= pdx * 0.2 * fps; }
-        if (mapW[ipy_sub_yo * mapX + ipx] == 0) { py -= pdy * 0.2 * fps; }
+        if (mapW[ipy * mapX + ipx_sub_xo] == 0) { px -= pdx * moveRate * fps; }
+        if (mapW[ipy_sub_yo * mapX + ipx] == 0) { py -= pdy * moveRate * fps; }
     }
     
-    glutPostRedisplay();
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     drawRays2D();
+    drawMonsterSprite();
 
+    
     if (!pressFireSprite || !showZoomSprite) {
         if (showZoomSprite) {
             drawZoomSprite();
@@ -321,6 +403,9 @@ void display()
         }
         drawFireSprite();
     }
+  
+   
+    glutPostRedisplay();
     glutSwapBuffers();
 }
 
@@ -331,7 +416,7 @@ void ButtonDown(unsigned char key, int x, int y)                                
     if (key == 'w') { Keys.w = 1; }
     if (key == 's') { Keys.s = 1; }
     if (key == 'q') { showZoomSprite = !showZoomSprite;}
-    if (key == 'r') { pressFireSprite= !pressFireSprite; }
+    if (key == 'r' && showZoomSprite == 1) { pressFireSprite= !pressFireSprite; }
     if (key == 'e')             //open doors
     {
         int xo = 0; if (pdx < 0) { xo = -25; }
