ab6cb129b8ffb43988967b77c7cd7f7a1767ee3b
Jason Park <142364450+DutchVandaline@users.noreply.github.com>
2024-05-01 20:48:34 +0900

    sprites.h must be change due to sprites.c changed

--- sprites.h
+++ sprites.h
@@ -7,6 +7,10 @@ extern int playerSprite2[32][32];
 extern int zoomSprite1[32][32];
 extern int zoomSprite2[32][32];
 extern int fireSprite[32][32];
+extern int monster_walkSprite1[];
+extern int monster_walkSprite2[];
+extern int monster_walkSprite3[];
+extern int monster_walkSprite4[];
 extern int All_Textures[];
 
 
